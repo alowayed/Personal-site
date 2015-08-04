e74c21a51e183fb61bdeb9199c25d8a69386c337
Yousef <Yousef@Yousefs-MacBook-Pro.local>
2015-08-04 21:05:30 +0300

    Nothing

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     
 
     $("blockquote").hover(function(){
-        $("blockquote").fadeTo("50000", 0.3);
+        $("blockquote").fadeTo("50000", 0.5);
         }, function(){
         $("blockquote").fadeTo("50000",1);
     });
