97e4453fd3748fc48e4c70a892f36926ef89e6e1
Yousef <Yousef@Yousefs-MacBook-Pro.local>
2015-08-06 20:26:17 +0300

    Add jokes and implement random joke/quote when clicked Still doesn't read from text file

--- script.js
+++ script.js
@@ -10,65 +10,55 @@ $(document).ready(function(){
   });
   /* Quote fade (end) */
 
+var quotes = [
+		'I told my girlfriend she drew her eyebrows to high. <br>She seemed surprised.',
+		'I bought some shoes from a drug dealer. \
+			<br>I don\'t know what he laced them with, but I\'ve been tripping all day.'
+	];
 
-  $("blockquote").click(function(){
-  	$(this).fadeTo("5000000", 0 ,function(){
-  			$(this).load('quotes.html');
-  			$(this).fadeTo("5000000", 0.5);
-  		});
-  });
+var jokes = [
+		'I told my girlfriend she drew her eyebrows to high. <br>She seemed surprised.',
+		'I bought some shoes from a drug dealer. \
+			<br>I don\'t know what he laced them with, but I\'ve been tripping all day.',
+		'I told my girlfriend she drew her eyebrows to high. <br>She seemed surprised.',
+		'I have the heart of a lion <br>and a lifetime ban from the San Diego Zoo.',
+		'What did one orphan say to the other? <br>"Robin, get in the Batmobile."',
+		'You heard the rumor going around about butter? <br>Never mind, I shouldn\'t spread it.',
+		'What did the Buddhist ask the hot dog vendor? <br>"Make me one with everything."\
+			The Buddhist gives him a fifty, and the vendor pockets it.\
+			The Buddhist asks for change and the vendor replies, <br>"Change comes from within."',
+		'What do you get when you cross a dyslexic an insomniac, and an agnostic? \
+			<br>Someone who lays awaks at night wondering if there is a dog.',
+		'And God said to John,\
+			<br>"Come forth and you shall be granted eternal life."\
+			<br>But John came fifth and won a toaster.'
+	];
+
+
+
+
+	/* Quote fade (start) */
+	$("#full-quote").click(function(){
+		$(this).fadeTo("5000000", 0 ,function(){
+			random_quote = quotes[Math.floor(Math.random() * quotes.length)];
+			$(this).children('blockquote').children('h3').html(random_quote);
+			$(this).fadeTo("5000000", 1);
+		});
+	});
+	/* Quote fade (start) */
+
+
+	$("#full-joke").click(function(){
+		$(this).fadeTo("5000000", 0 ,function(){
+			random_joke = jokes[Math.floor(Math.random() * jokes.length)];
+			$(this).children('blockquote').children('h3').html(random_joke);
+			$(this).fadeTo("5000000", 1);
+		});
+	});
   
 
 
-  /*
-	var xmlhttp = null; 
 
-	function AjaxRequest(url){ 
-		if(xmlhttp != null){ 
-			if(xmlhttp.abort) 
-			xmlhttp.abort(); 
-			xmlhttp = null; 
-		};
-
-		if(window.XMLHttpRequest) // good browsers 
-			xmlhttp=new XMLHttpRequest(); 
-		else if(window.ActiveXObject) // IE 
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); 
-	
-		if(xmlhttp == null) 
-			return null; 
-
-		xmlhttp.open("GET",url,false); 
-		xmlhttp.send(null); 
-
-		if(xmlhttp.status >= 200 && xmlhttp.status < 300)// 2xx is good enough 
-			return xmlhttp.responseText; 
-		else 
-			return null; 
-	} 
-
-	function clearEle(element){ 
-		while(element.hasChildNodes()) 
-			element.removeChild(element.firstChild); 
-	} 
-
-	window.onload = function(){ 
-		var el, msg, date, txt = AjaxRequest('jokes.txt'); 
-		if(txt == null) 
-			msg = 'page not found or Ajax not supported by your browser.'; 
-		else{ 
-			msg = txt;
-		} 
-		date = new Date(); 
-		txt = msg.split("_________"); 
-		date = Math.floor(date.getTime() / (24*60*60*1000)); 
-
-		el = document.getElementById('quote'); 
-
-		clearEle(el); 
-		el.appendChild( document.createTextNode(txt[Math.floor((Math.random()*txt.length)]))); 
-	} 
-	*/
     
 
 });
