4f2fb388186233e30ddd3bb7bd56e936ebe45874
Yousef <Yousef@Yousefs-MacBook-Pro.local>
2015-08-16 18:03:27 +0300

    Nothing

--- script.js
+++ script.js
@@ -88,30 +88,42 @@ $(document).ready(function(){
 			});
 			/* Fix nav-bar while scrolling (end) */
 
-			/* Fade in CS-section (start) */
-			var toActivateCS = $('#section-CS').visible(true);
-			var toActivateCSChild = $('#section-CS div').visible(true);
-			if (toActivateCS || toActivateCSChild) {
-				$('#sidenav-CS').fadeIn(500);
-		    } else {
-		    	$('#sidenav-CS').fadeOut(500, function(){
-		    		$('#sidenav-CS').hide();
-		    	});
-		    }
-		    /* Fade in CS-section (end) */
+			var CSTop = $('#section-CS').offset().top;
+			var EETop = $('#section-EE').offset().top;
+			var MechETop = $('#section-MechE').offset().top;
+			var LeadTop = $('#section-Lead').offset().top;
 
-		    /* Fade in EE-section (start) */
-			var toActivateCS = $('#section-EE').visible(true);
-			var toActivateCSChild = $('#section-EE div').visible(true);
-			if (toActivateCS || toActivateCSChild) {
-				$('#sidenav-EE').fadeIn(500);
-		    } else {
-		    	$('#sidenav-EE').fadeOut(500, function(){
+			/* Fade in CS-section, fade out EE-section (start) */
+			if ((currentScroll >= CSTop) && (currentScroll <= (EETop - screen.height/2))) {
+				$('#sidenav-CS').fadeIn(500);
+				$('#sidenav-EE').fadeOut(500, function(){
 		    		$('#sidenav-EE').hide();
 		    	});
-		    }
-		    /* Fade in EE-section (end) */
+			}
+			/* Fade in CS-section, fade out EE-section (end) */
 
+			/* Fade in EE-section, fade out CS and MechE-section (start) */
+			else if ((currentScroll >= (EETop - screen.height/2)) && (currentScroll <= (MechETop - screen.height/2))){
+				$('#sidenav-CS').fadeOut(500, function(){
+		    		$('#sidenav-CS').hide();
+		    	});
+				$('#sidenav-EE').fadeIn(500);
+				$('#sidenav-MechE').fadeOut(500, function(){
+		    		$('#sidenav-MechE').hide();
+		    	});
+			}
+			/* Fade in EE-section, fade out CS and MechE-section (end) */
+
+			/* Fade in MechE-section, fade out EE and Lead-section (start) */
+			else if ((currentScroll >= (MechETop - screen.height/2)) && (currentScroll <= (LeadTop - screen.height/2))){
+				$('#sidenav-EE').fadeOut(500, function(){
+		    		$('#sidenav-EE').hide();
+		    	});
+				$('#sidenav-MechE').fadeIn(500);
+				$('#sidenav-Lead').fadeOut(500, function(){
+		    		$('#sidenav-Lead').hide();
+		    	});
+			}
 		    
 	    }
 	    /* When scrolling through content (end) */
